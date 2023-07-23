ef780fc4686558baac72fc872c98fc05ae5730f4
Priyanshu-bit <priyanshupatel56@gmail.com>
2023-07-24 03:39:10 +0530

    sdf

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 
-const API_URI = "";
+
 
 export const uploadFile = async (data) => {
     try {
-        const response = await axios.post(`${API_URI}/upload`, data);
+        const response = await axios.post(`${process.env.API_URI}/upload`, data);
         return response.data;
     } catch (error) {
         console.log('Error while calling the API ', error.message);
