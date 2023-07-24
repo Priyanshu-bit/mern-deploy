118e25e7946411c823f8f930432cc77b60010390
Priyanshu-bit <priyanshupatel56@gmail.com>
2023-07-24 11:05:18 +0530

    ihhskdjlhfglsdjkgf

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
 
-
-
+const API_URI="22AimwKMNYcH6NpJ9fNdrlJiOQpP0iKSoOjxy30KlqpULcXZL4Nzzg3SD6gOanbD"
 export const uploadFile = async (data) => {
     try {
-        const response = await axios.post(`${process.env.API_URI}/upload`, data);
+        const response = await axios.post(`${API_URI}/upload`, data);
         return response.data;
     } catch (error) {
         console.log('Error while calling the API ', error.message);
--- controller/image-controller.js
+++ controller/image-controller.js
@@ -12,7 +12,7 @@ export const uploadImage = async (request, response) => {
     
     try {
         const file = await File.create(fileObj);
-        response.status(200).json({ path:  `https://ill-jade-bonobo-tux.cyclic.app/file/${file._id}`});
+        response.status(200).json({ path: `https://ill-jade-bonobo-tux.cyclic.app//file/${file._id}`});
     } catch (error) {
         console.error(error.message);
         response.status(500).json({ error: error.message });
@@ -23,7 +23,7 @@ export const getImage = async (request, response) => {
     try {   
         const file = await File.findById(request.params.fileId);
         
-        file.downloadCount++;
+        file.downloadContent++;
 
         await file.save();
 
