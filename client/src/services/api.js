0f76bc48dfe4374224852c0f917aad6779710abc
Priyanshu-bit <priyanshupatel56@gmail.com>
2023-07-25 21:38:04 +0530

    final deply

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_URI=`http://0.0.0.0/${process.env.PORT}`
+const API_URI="http://localhost:8000"
 export const uploadFile = async (data) => {
     try {
         const response = await axios.post(`${API_URI}/upload`, data);
