6e99b008579e59cc839088339f1ee35c30e8e120
Priyanshu-bit <priyanshupatel56@gmail.com>
2023-07-25 22:03:37 +0530

    deploy

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,12 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
-
-const API_URI=""
+const API_URI = "";
 export const uploadFile = async (data) => {
-    try {
-        const response = await axios.post(`${process.env.API_URI}/upload`, data);
-        return response.data;
-    } catch (error) {
-        console.log('Error while calling the API ', error.message);
-    }
-}
+  try {
+    const response = await axios.post(
+      `https://filefly-be.onrender.com/upload`,
+      data
+    );
+    return response.data;
+  } catch (error) {
+    console.log("Error while calling the API ", error.message);
+  }
+};
